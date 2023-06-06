d887a2d998d03fc2c8a568df5c223d6f2f9cb05c
Tencha <114583193+codakawa@users.noreply.github.com>
2023-06-06 16:29:40 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const poems = ['Мороз', 'и', 'солнце', 'день', 'чудесный
 const userNum = Number(prompt("Num? (1-5)"))
 const newWord = prompt('New word?')
 
-// let newPoems = poems.filter((el) => el !== poems[userNum-1])
 poems.splice(userNum-1, 1, newWord);
 
 console.log(poems);
